1d5e922f75af7358fdbf143bff5ff031730bbcc6
mingo <mingo233@qq.com>
2024-01-11 18:31:22 +0800

    feat: dayapi

---
+++ utils/dayApiRobot.js
@@ -0,0 +1,26 @@
+// day机器人通知
+const { dayRobotApi, initDayHttpAxios } = require('../api/dayAPi')
+
+class dayBot {
+    token
+    http
+    constructor(token) {
+        this.token = token
+        this.http = initDayHttpAxios(token)
+    }
+    sendSignTipMsg() {
+        return this.http.get(dayRobotApi.sendSignMsg)
+    }
+
+}
+const fsBotApp = new dayBot()
+
+module.exports = fsBotApp
+
+// 调试
+// let fsBot = new FsBot()
+fsBotApp.sendSignTipMsg().then(res => {
+    console.log(res);
+}).catch(err => {
+    console.log('err', err);
+})
